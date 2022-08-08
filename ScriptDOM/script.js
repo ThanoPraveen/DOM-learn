17810a0362781c008d0dd12399fb3b74aaa49882
MPK PRAVEEN KUMAR <mpkthanos428@gmail.com>
2022-08-08 08:21:44 +0530

    practice GEBI

--- ScriptDOM/script.js
+++ ScriptDOM/script.js
@@ -1,14 +1,15 @@
 // 1. getElementById selected and find the ID selector
-// let mainHeading = document.getElementById("mainHeading");
-// mainHeading.innerHTML = "Hello Everyone, DOM &#128521;";
+let mainHeading = document.getElementById("mainHeading");
+mainHeading.innerHTML = "Hello Everyone, DOM &#128521;";
 
-// let inputFoodId = document.getElementById("input-food-id");
-// let foodBtn = document.getElementById("food-btn");
-// let addInputValue = document.getElementById("add-input-value");
-// // console.log(inputFoodId, foodBtn);
+let inputFoodId = document.getElementById("input-food-id");
+let foodBtn = document.getElementById("food-btn");
+let addInputValue = document.getElementById("add-input-value");
+// console.log(inputFoodId, foodBtn);
 
-// foodBtn.addEventListener("click", () => {
-//   // addInputValue.innerText = inputFoodId.Value;
-//   addInputValue.innerText = inputFoodId.Value;
-//   console.log(inputFoodId.value);
-// });
+foodBtn.addEventListener("click", () => {
+    // addInputValue.innerText = inputFoodId.Value;
+    addInputValue.append(inputFoodId.value);
+    // console.log(inputFoodId.value);
+    // console.log(addInputValue);
+});
